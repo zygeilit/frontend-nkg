7fa6f95d03d8935c59786f6a70c361723758eb99
zhangyue <zhangyue@beisen.com>
2019-06-03 13:06:39 +0800

    contains times

--- packages/algorithm/examples/binary-search-tree/binary-search-tree.js
+++ packages/algorithm/examples/binary-search-tree/binary-search-tree.js
@@ -1,11 +1,12 @@
 
+let containsInvokeTimes = 0
+
 // 二分搜索树
 // 1. 每个节点大于左子树的所有节点的值
 // 2. 每个节点小于又子树的所有节点
 // 3. 每一颗子树也是二分搜索树
 
 // 存储的元素必须有可比较性
-
 
 export class Node {
 
@@ -43,7 +44,7 @@ export default class BinarySearchTree {
     // }
 
     // 优化后
-    this.root = thia._add_private(this.root, e)
+    this.root = this._add_private(this.root, e)
   }
 
   // 向以node为根的二分搜索树中插入元素E，递归算法
@@ -81,11 +82,14 @@ export default class BinarySearchTree {
     return node
   }
 
+  // 包含
   contains(e) {
-    return this.contains(this.root, e)
+    return this._contains_private(this.root, e) && console.log(`函数查找次数（contains）：${containsInvokeTimes}`)
   }
-
   _contains_private(node, e) {
+
+    // 计数，查找结点，递归执行次数
+    containsInvokeTimes++
 
     if (node == null) {
       return false
@@ -100,11 +104,40 @@ export default class BinarySearchTree {
     }
   }
 
+  // 后续遍历
+  // 先处理子节点，如：释放内存
+  postOrder() {
+    return this._postOrder_private(this.root)
+  }
+  _postOrder_private() {
+    if (node == null) {
+      return
+    }
+
+    this._postOrder_private(node.left)
+    this._postOrder_private(node.right)
+    console.log(node.e)
+  }
+
+  // 中序遍历
+  // 从小到大排序
+  inOrder() {
+    return this._inOrder_private(this.root)
+  }
+  _inOrder_private(node) {
+    if (node == null) {
+      return
+    }
+
+    this._inOrder_private(node.left)
+    console.log(node.e)
+    this._inOrder_private(node.right)
+  }
+
   // 前序遍历
   preOrder() {
     return this._preOrder_private(this.root)
   }
-
   _preOrder_private(node) {
 
     if (node == null) {
@@ -112,8 +145,7 @@ export default class BinarySearchTree {
     }
 
     console.log(node.e)
-
-    this._traverse_private(node.left)
-    this._traverse_private(node.right)
+    this._preOrder_private(node.left)
+    this._preOrder_private(node.right)
   }
 }
--- packages/algorithm/examples/binary-search-tree/index.js
+++ packages/algorithm/examples/binary-search-tree/index.js
@@ -1,81 +1,30 @@
 
 import React, { Component } from 'react'
-
-class Popup {
-
-  constructor(items) {
-    this.items = items || []
-  }
-
-  sort() {
-    let len = this.items.length
-    let items = this.items
-
-    for (let i=0; i<len; i++) {
-      for (let j=0; j<len - i; j++) {
-        if (items[j+1] < items[j]) {
-          let temp = items[j+1]
-          items[j+1] = items[j]
-          items[j] = temp
-        }
-      }
-    }
-
-    return items
-  }
-}
+import BST from './binary-search-tree'
 
 export default class extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      'values': [ 1,2,23,2342,3,34,345,35,35,5,555,6,7,7,7,7878,789,8 ],
-      'elements': []
-    }
-  }
-
-  onChangeHanlder(e) {
-    this.setState({
-      'values': e.target.value.split(',')
-    })
-  }
-
-  onClickHanlder() {
-    let pop = new Popup(this.state.values)
-    let elements = pop.sort()
-    this.setState({
-      elements
-    })
-  }
-
   componentDidMount() {
-    let pop = new Popup(this.state.values)
-    let elements = pop.sort()
-    this.setState({
-      elements
-    })
+
+    let items = [ 1, 123123,12312,4,3423,5,,432,12,3,54,8,9,,45,34,45,678,45,23,123,12,31234,235,3453,46,4567]
+    console.log(`数组长度：` + items.length)
+
+    const bst = new BST()
+    
+    for(let i=0; i<items.length; i++) {
+      bst.add(items[i])
+    }
+    
+    console.log(`使用 contains() 函数查找节点是否存在：` + 31234)
+    bst.contains(31234)
+
+    console.log(`二分查找树，中序遍历，排序：`)
+    bst.inOrder()
   }
 
   render () {
     return <div>
-
-      <div>输入框：</div>
-      <input
-        style={{
-          width: 500,
-          height: 40,
-          fontSize: 19,
-          marginBottom: 15 
-        }}
-        onChange={this.onChangeHanlder.bind(this)}
-        value={this.state.values}></input>
-      <input type="button" value="sort" onClick={this.onClickHanlder.bind(this)} />
-
-      <div>排序后值：</div>
-      <div>{
-        JSON.stringify(this.state.elements, null, 2).replace(/\"/g, '')
-      }</div>
+      <div>二分查找树分析，见开发者工具 -> 控制台</div>
     </div>
   }
 }
