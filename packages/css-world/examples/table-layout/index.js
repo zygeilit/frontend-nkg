e311a08b7e607d685d5a6719cc20e5f3c662fa80
Zhangyue <ZhangYue>
2020-12-28 17:33:07 +0800

    table tayout

---
+++ packages/css-world/examples/table-layout/index.js
@@ -0,0 +1,32 @@
+
+import React, { Component } from 'react'
+import './index.scss'
+
+export default class extends Component {
+  render() {
+    return <div className="table-layout">
+      <table>
+        <thead>
+          <tr>
+            <th>人员</th>
+            <th>部门</th>
+            <th>考核模板</th>
+            <th>当前阶段</th>
+            <th>维度一</th>
+            <th>考核结果</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式设计师专用正式</td>
+            <td>默认部门</td>
+            <td>OKR考核模板OKR考核模板OKR考核模板OKR考核模板OKR考核模板</td>
+            <td>员工自评</td>
+            <td>--</td>
+            <td>--</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  }
+}
